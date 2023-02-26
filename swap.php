8eb18001c1f106974603ee0284604c5294218358
Yokess s <yokess025@gmail.com>
2023-02-26 09:41:20 +0000

    first commit

---
+++ swap.php
@@ -0,0 +1,14 @@
+<?php
+$num1 = 20;
+$num2 = 30;
+ 
+echo "\nThe number before swapping is:\n";
+echo "num1 = ".$num1." and num2= ".$num2 ;
+ 
+$temp = $num1;
+$num1 = $num2;
+$num2 = $temp;
+ 
+echo "\nThe number after swapping is: \n";
+echo "num1 = ".$num1." and num2= ".$num2."\n";
+?>
